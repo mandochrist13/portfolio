eac56b53b8e2f4414021f110608fd947f4642805
mando_christ <mandochrist13@gmail.com>
2024-10-11 23:39:53 +0100

    fix bug

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -7,7 +7,7 @@ export default function footer() {
         
         <a
           className="flex text-center px-4 items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://portfolio-mando-blush.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
